684af56f91ace443d20122404e6f2b3fd45ddb9a
QIANG DING <joshua.qj@hotmail.com>
2023-04-11 14:01:12 +1000

    Add Site To PrinterModel

---
+++ PrinterManagementAppLibrary/Migrations/20230411033256_ini5.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrinterManagementAppLibrary.Migrations
+{
+    public partial class ini5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Printers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Department",
+                table: "Printers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Site",
+                table: "Printers",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Site",
+                table: "Printers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Printers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Department",
+                table: "Printers",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+    }
+}
